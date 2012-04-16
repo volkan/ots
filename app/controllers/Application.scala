5c794f850417bdc4a81017080fc32520a919e0cc
Volkan Altan <volkanaltan@gmail.com>
2012-04-16 17:15:58 +0300

    test amaçlı işlemler

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,59 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.Play.current
+import play.i18n.Messages;
+import jsmessages.JsMessages
+
+
+import models._
+import views._
 
 object Application extends Controller {
-  
-  def index = Action {
+
+  val loginForm = Form(
+    tuple(
+      "email" -> text,
+      "password" -> text
+    ) verifying ("Invalid email or password", result => result match {
+      case (email, password) => true
+    })
+  )
+
+  def login2 = Action { implicit request =>
+    session +("success" -> "TESTTTTT");
+    Redirect(routes.Application.login).withNewSession.flashing(
+      "success" -> "Yönlendirme ile mesaj çıkıyor"
+    )
+  }
+  /**
+   * Login page.
+   */
+  def login = Action { implicit request =>
+    session +("success" -> "TESTTTTT");
+    flash +("success" -> "TESTTTTT");
+    Ok(html.login(loginForm))
+  }
+
+  def authenticate = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def index2 = Action {
+    Ok(views.html.index("Your new application is ready."))
+  }
+
+  def index = Action { implicit request =>
+
+    //flash.+("success" -> Messages.get("youve.been.logged.out"));
+    //flash("success" -> Messages.get("youve.been.logged.out"));
+    Ok(views.html.index("Your new application is ready."))
+  }
+
+  def jsMessages = Action { implicit request =>
+    Ok(JsMessages("Messages")).as(JAVASCRIPT)
   }
   
 }
--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,15 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+      "postgresql" % "postgresql" % "9.1-901-1.jdbc4",
+      //"siena" % "play-siena" % "v2.0.2"
+      "com.github.julienrf" %% "play-jsmessages" % "1.0",
+      "com.typesafe" %% "play-plugins-mailer" % "2.0"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+      resolvers += "Apache" at "http://repo1.maven.org/maven2/",
+      resolvers += "julienrf.github.com" at "http://julienrf.github.com/repo/"
     )
 
 }
